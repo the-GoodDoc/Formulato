fd321d6bd9c8b0942c7d84805dfd2acc675ddfbd
karimAlstura <digiyib392@codgal.com>
2026-02-08 00:39:02 +0200

    Add files via upload

---
+++ frontend/assits/js/main.js
@@ -0,0 +1,16 @@
+function updateTimeAndDate() {
+        const footerTime = document.getElementById('footer_time');
+        const footerDate = document.getElementById('footer_date');
+
+        if (footerTime && footerDate) {
+            const now = new Date();
+            footerTime.textContent = now.toLocaleTimeString();
+            footerDate.textContent = now.toLocaleDateString();
+        }
+    }
+
+    // This makes it automatic every second
+    setInterval(updateTimeAndDate, 1000);
+    
+    // Run immediately
+    updateTimeAndDate();
